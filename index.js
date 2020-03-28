63e804aad51949dd24c4d153ed7f4e1a75f69a7c
Ian Holmes <ihh@berkeley.edu>
2020-03-27 23:43:52 -0700

    Trajectory likelihood computations

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const machineboss = require ('machineboss')
+const machineboss = require ('machineboss'),
+      { initPromise, runWithFilesSync } = machineboss
 
 // Algorithm 1 of (Miklos, Lunter & Holmes, 2004)
 // exitRates = chi
 // transitionRates = r
 const trajectoryLikelihood = (exitRates, transitionRates, time) => {
-  if (T <= 0)
+  if (typeof(time) !== 'number')
+    throw new Error ("Time must be numeric")
+  if (time <= 0)
     throw new Error ("Trajectory must have finite duration")
   if (exitRates.length === 0)
     throw new Error ("There must be at least one state in the trajectory")
-  if (exitRates.length + 1 !== transitionRates.length)
+  if (exitRates.filter ((r) => typeof(r) !== 'number').length)
+    throw new Error ("Exit rates must be numeric")
+  if (exitRates.length !== transitionRates.length + 1)
     throw new Error ("Mismatch between numbers of exit rates & transition rates")
-  if (transitionRates.filter ((r) => r < 0))
+  if (transitionRates.filter ((r) => typeof(r) !== 'number').length)
+    throw new Error ("Transition rates must be numeric")
+  if (transitionRates.filter ((r) => r < 0).length)
     throw new Error ("Transition rates must all be nonnegative")
-  if (transitionRates.filter ((r, i) => r <= exitRates[i]))
+  if (transitionRates.filter ((r, i) => r > exitRates[i]).length)
     throw new Error ("Exit rates must be at least as great as transition rates")
   // switch to the notation from the paper
   let zeta = exitRates,
@@ -21,7 +28,7 @@ const trajectoryLikelihood = (exitRates, transitionRates, time) => {
       chi = [exitRates[0]],  // unique exit rates
       d = [0],  // exit rate degeneracy
       c = [[1]]   // c[n][k] = coefficient of T^k exp(-chi[n]*T) in likelihood
-  zeta.slice(1).forEach ((zeta_i, i) => {
+  zeta.slice(1).forEach ((zeta_i) => {
     const j = chi.indexOf (zeta_i)
     if (j < 0) {
       chi.push (zeta_i)
@@ -29,19 +36,19 @@ const trajectoryLikelihood = (exitRates, transitionRates, time) => {
     } else
       ++d[j]
     let u = [],  // u[n][k] = new value of c[n][k]
-        M = chi.length + 1
+        M = chi.length - 1
     chi.forEach ((chi_n, n) => {
       let u_n = []
       for (let k = 0; k <= d[n]; ++k) {
         let u_nk = 0
         if (chi_n != zeta_i)
           for (let j = k; j <= d[n]; ++j)
-            u_nk -= c[n][j] * factorial(j) / (factorial(k) * Math.pow (chi[n] - zeta[i], j - k + 1))
+            u_nk -= c[n][j] * factorial(j) / (factorial(k) * Math.pow (chi[n] - zeta_i, j - k + 1))
         else if (k == 0) {
           for (let m = 0; m <= M; ++m)
             if (m != n)
               for (let j = k; j <= d[m]; ++j)
-                u_nk += c[m][j] * factorial(j) / Math.pow (chi[m] - zeta[i], j+1)
+                u_nk += c[m][j] * factorial(j) / Math.pow (chi[m] - zeta_i, j+1)
         } else
           u_nk = c[n][k-1] / k
         u_n.push (u_nk)
@@ -51,7 +58,7 @@ const trajectoryLikelihood = (exitRates, transitionRates, time) => {
     c = u
   })
   let waitProb = 0,
-      M = chi.length + 1
+      M = chi.length - 1
   for (let n = 0; n <= M; ++n) {
     let T_poly = 0
     for (let k = 0; k <= d[n]; ++k)
@@ -72,21 +79,34 @@ const factorial = (n) => {
 
 // The trajectory in Figure 1 of MLH2004 is
 //  AAAM -> AABBBBAM -> BBAM -> BBM
-// The zone lengths are
-//  4 -> 8 -> 4 -> 3
-const indelTrajectoryLikelihood = async (zoneLengths, params, time) => {
+// The zone lengths for this trajectory are
+//  3 -> 7 -> 3 -> 2
+// Note that there are other valid trajectories with these zone lengths, e.g.
+//  AAAM -> AAABBBBM -> ABBM -> BBM
+// There are also invalid trajectories with the same zone lengths, e.g.
+//  AAAM -> AAABBBBM -> AAAM -> AAM  (not allowed; we can't have any A's left at the end)
+// We count the number of valid trajectories using a finite-state machine approach.
+const indelTrajectoryLikelihood = (zoneLengths, params, time) => {
   const { gamma, mu, r } = params
   const N = zoneLengths.length
+  if ([gamma,mu,r].filter ((p) => typeof(p) !== 'number').length)
+    throw new Error ("Parameters {gamma,mu,r} must be defined numeric")
+  if (typeof(time) !== 'number')
+    throw new Error ("Time must be numeric")
+  if (time < 0)
+    throw new Error ("Time must be nonnegative")
   if (N === 0)
     throw new Error ("There must be at least one state in the trajectory")
-  if (zoneLengths.filter ((l) => l < 0).length)
-    throw new Error ("All zone lengths must be nonnegative")
+  if (zoneLengths.filter ((l) => typeof(l) !== 'number').length)
+    throw new Error ("Zone lengths must be numeric")
+  if (zoneLengths.filter ((l) => l < 0 || Math.round(l) != l).length)
+    throw new Error ("All zone lengths must be nonnegative integers")
   if (zoneLengths.filter ((l, n) => l == 0 && n < N-1).length)
     throw new Error ("Only the final zone length in the trajectory can be zero")
   if (zoneLengths.filter ((l, n) => n < N-1 && l == zoneLengths[n+1]).length)
     throw new Error ("No two adjacent states in the trajectory can be identical")
-  const exitRates = zoneLengths.map ((l) => exitRateForZoneLength(l))
-  const transitionRates = zoneLengths.slice(1).map ((l, n) => transitionRateForZoneLengthChange (zoneLengths[n-1], l, params))
+  const exitRates = zoneLengths.map ((l) => exitRateForZoneLength (l, params))
+  const transitionRates = zoneLengths.slice(1).map ((l, n) => transitionRateForZoneLengthChange (zoneLengths[n], l, params))
   return trajectoryLikelihood (exitRates, transitionRates, time) * indelTrajectoryDegeneracy (zoneLengths)
 }
 
@@ -94,7 +114,7 @@ const exitRateForZoneLength = (zoneLength, params) => {
   const { gamma, mu, r } = params
   const lambdaSum = gamma * mu * (1-r) * (1-r) / (1 - gamma*r)
   const muSum = mu * (1-r)
-  return zoneLength * (lambdaSum + muSum)
+  return (zoneLength + 1) * (lambdaSum + muSum)
 }
 
 const insertionRate = (k, params) => {
@@ -110,60 +130,40 @@ const deletionRate = (k, params) => {
 const transitionRateForZoneLengthChange = (srcZoneLength, destZoneLength, params) => {
   const { gamma, mu, r } = params
   return (destZoneLength > srcZoneLength
-          ? insertionRate (destZoneLength - srcZoneLength)
-          : deletionRate (destZoneLength - srcZoneLength))
+          ? insertionRate (destZoneLength - srcZoneLength, params)
+          : deletionRate (srcZoneLength - destZoneLength, params))
 }
 
-const indelTrajectoryDegeneracy = async (zoneLengths) => {
-  let eventMachine = []
+const indelTrajectoryDegeneracy = (zoneLengths) => {
+  let machines = []
   for (let n = 0; n < zoneLengths.length - 1; ++n) {
     const deltaLength = zoneLengths[n+1] - zoneLengths[n]
-    eventMachine.push (deltaLength > 0
-                       ? makeInsertionMachine (deltaLength)
-                       : makeDeletionMachine (deltaLength))
+    machines.push (deltaLength > 0
+                   ? makeInsertionMachine (deltaLength)
+                   : makeDeletionMachine (-deltaLength))
   }
-  const inputSeq = String.repeat ("A", zoneLengths[0])
-  const outputSeq = String.repeat ("X", zoneLengths[zoneLengths.length - 1])
-  const mbResult = await machineboss.runWithFiles (eventMachine
-                                                   .concat (['--input-chars', inputSeq,
-                                                             '--output-chars', outputSeq,
-                                                             '--loglike']))
-  const mbJson = JSON.parse (mbResult.stdout)
-  return Math.exp (mbJson[0][2])
-}
-
-const extend = function() {
-  let a = arguments[0]
-  Array.from(arguments).slice(1).forEach ((b) => Object.keys(b).forEach ((k) => a[k] = b[k]))
-  return a
-}
-
-const makeTransition = (protoTrans, dest) => {
-  return extend ({}, protoTrans, { to: dest })
-}
-
-// Ancestral residue = "A"
-// Inserted residue = "X"
-const makeMachine = (protoTrans, copies) => {
-  let state = []
-  for (let n = 0; n < copies; ++n)
-    state.push (extend ({ id: n,
-                          trans: ((n ? [] : [{ to: 0, in: "A", out: "A" },
-                                             { to: 0, in: "X", out: "X" }])
-                                  .concat (protoTrans.map (pt => makeTransition (pt, n+1)))) }))
-  state.push ({ id: copies, trans: [{ to: copies, in: "A", out: "A" },
-                                    { to: copies, in: "X", out: "X" }] })
-  return { state }
+  const inputSeq = "A".repeat (zoneLengths[0])
+  const outputSeq = "B".repeat (zoneLengths[zoneLengths.length - 1])
+  const args = machines.join(" --compose ").split(" ")
+        .concat (["--input-chars", inputSeq,
+                  "--output-chars", outputSeq,
+                  "--loglike"])
+  const mbResult = runWithFilesSync (args)
+  const mbJson = JSON.parse (mbResult.stdout),
+        logForwardProb = mbJson[0][2]
+  return logForwardProb == '-Infinity' ? 0 : Math.round(Math.exp(logForwardProb))
 }
 
 const makeInsertionMachine = (k) => {
-  return makeMachine ([{ out: "X" }], k)
+  return "--begin --echo-wild AB --concat --generate-chars " + "B".repeat(k) + " --concat --echo-wild AB --end"
 }
 
 const makeDeletionMachine = (k) => {
-  return makeMachine ([{ in: "A" }, { in: "X" }], k)
+  return "--begin --echo-wild AB --concat --begin --recognize-one AB --repeat " + k + " --end --concat --echo-wild AB --end"
 }
 
 module.exports = { trajectoryLikelihood,
-                   indelTrajectoryLikelihood,
-                   factorial }
+                   factorial,
+                   initPromise,
+                   indelTrajectoryDegeneracy,
+                   indelTrajectoryLikelihood }
